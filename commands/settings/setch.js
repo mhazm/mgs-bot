29aa10cf904a7db77d19e936d7325cedb9bddf53
Muhammad Hazmi <mhazmiee@gmail.com>
2022-04-03 09:54:47 +0700

    Fix only member has Administrator permission

--- commands/settings/setch.js
+++ commands/settings/setch.js
@@ -8,71 +8,165 @@ module.exports = {
     usage: "<setting name> <channel>",
     category: "settings",
     run: async(client, message, args) => {
-        let data = await Guild.findOne({
-            guildID: message.guild.id
-        });
+        try {          
+            if (!message.member.permissions.has([Discord.Permissions.FLAGS.BAN_MEMBERS]))
+            return message.reply(`Siapa lu woy!!`);
 
-        // Get modlog Channel
-        const modlog = client.channels.cache.get(data.modlogChannel);
-
-        if(!args[0]) {
-            let err = new Discord.MessageEmbed()
-                .setDescription('Kamu harus menggunakan arguments untuk mensetting channel.\nContoh: !setch <args> <channel>\n\nList Argument bisa dilihat dibawah ini')
-                .addField('List Agruments', '1. welcome\n2. modlog')
-                .setColor(client.config.warning)
-            return message.channel.send({ embeds: [err] });
-        }
-
-        if (args[0].toLowerCase() === 'welcome') {
-            let channel =  message.mentions.channels.first();
-            if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
-            let embed = new Discord.MessageEmbed()
-                .setDescription(`Berhasil setting welcome channel di ${channel}`)
-                .setTimestamp(new Date())
-                .setColor(client.config.berhasil)
-            message.channel.send({ embeds: [embed] });
-            data.channel.welcome = channel.id; data.save();
-
-            // Send Report to ModLog
-            let report = new Discord.MessageEmbed()
-                .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
-                .setTimestamp(new Date())
-                .setColor(client.config.berhasil)
-            modlog.send({ embeds: [report] });
-        } else
-        if (args[0].toLowerCase() === 'bye') {
-            let channel =  message.mentions.channels.first();
-            if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
-            let embed = new Discord.MessageEmbed()
-                .setDescription(`Berhasil setting bye channel di ${channel}`)
-                .setTimestamp(new Date())
-                .setColor(client.config.berhasil)
-            message.channel.send({ embeds: [embed] });
-            data.channel.bye = channel.id; data.save();
-
-            // Send Report to ModLog
-            let report = new Discord.MessageEmbed()
-                .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
-                .setTimestamp(new Date())
-                .setColor(client.config.berhasil)
-            modlog.send({ embeds: [report] });
-        } else
-        if (args[0].toLowerCase() === 'modlog') {
-            let channel =  message.mentions.channels.first();
-            if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
-            let embed = new Discord.MessageEmbed()
-                .setDescription(`Berhasil setting modlog channel di ${channel}`)
-                .setTimestamp(new Date())
-                .setColor(client.config.berhasil)
-            message.channel.send({ embeds: [embed] });
-            data.channel.modlog = channel.id; data.save();
-
-            // Send Report to ModLog
-            let report = new Discord.MessageEmbed()
-                .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
-                .setTimestamp(new Date())
-                .setColor(client.config.berhasil)
-            modlog.send({ embeds: [report] });
-        }
+            let data = await Guild.findOne({
+                guildID: message.guild.id
+            });
+    
+            // Get modlog Channel
+            const modlog = client.channels.cache.get(data.channel.modlog);
+            if (!modlog) return;
+    
+            if(!args[0]) {
+                let err = new Discord.MessageEmbed()
+                    .setDescription('Kamu harus menggunakan arguments untuk mensetting channel.\nContoh: !setch <args> <channel>\n\nList Argument bisa dilihat dibawah ini')
+                    .addField('List Agruments', '1. welcome\n2. modlog')
+                    .setColor(client.config.warning)
+                return message.channel.send({ embeds: [err] });
+            }
+    
+            if (args[0].toLowerCase() === 'welcome') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting welcome channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.welcome = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'bye') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting bye channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.bye = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'modlog') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting modlog channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.modlog = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'afk') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting modlog channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.afk = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'story') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting modlog channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.story = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'income') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting modlog channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.income = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'chatmoney') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting modlog channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.chatmoney = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            } else
+            if (args[0].toLowerCase() === 'levelup') {
+                let channel =  message.mentions.channels.first();
+                if(!channel) return message.channel.send("Tolong di tag dulu channelnya!");
+                let embed = new Discord.MessageEmbed()
+                    .setDescription(`Berhasil setting modlog channel di ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                message.channel.send({ embeds: [embed] });
+                data.channel.levelup = channel.id; data.save();
+    
+                // Send Report to ModLog
+                let report = new Discord.MessageEmbed()
+                    .setDescription(`User **${message.author.username}** mengubah welcome channel ke ${channel}`)
+                    .setTimestamp(new Date())
+                    .setColor(client.config.berhasil)
+                modlog.send({ embeds: [report] });
+            }
+        } catch (error) {
+            return message.channel.send({ content: `Something went wrong: ${error.message}` });
+            // Restart the bot
+        };
     },
 };
