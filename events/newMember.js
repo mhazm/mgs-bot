3cd50e217e4be5bb95d80dd059738b7db8e354f2
Muhammad Hazmi <mhazmiee@gmail.com>
2022-04-08 09:54:36 +0700

    Add Timeout Message

--- events/newMember.js
+++ events/newMember.js
@@ -1,6 +1,7 @@
 const client = require('../index.js');
 const Discord = require('discord.js');
 const Guild = require('../models/Guild.js');
+const User = require('../models/User.js');
 
 client.on('guildMemberAdd', async(member) => {
     try {
@@ -17,14 +18,31 @@ client.on('guildMemberAdd', async(member) => {
         const channel = member.guild.channels.cache.get(data.channel.welcome);
         if (!channel) return;
 
+        let user = await User.findOne({
+            guildID: member.guild.id,
+            userID: member.user.id,
+        });
+
+        if (!user) {
+            const account = {
+                username: member.user.username,
+                userId: member.user.id,
+            }
+            User.create({
+                account,
+                guildID: member.guild.id,
+                userID: member.user.id,
+            })
+        };
+
         // Embed Creator
         const embed = new Discord.MessageEmbed()
-        .setTitle(`Welcome ${member.user.tag}`)
+        .setTitle(`Welcome ${member.user.username}`)
         .setColor(client.config.colorEmbed)
-        .setDescription(`Hei ${member.user.tag}, Selamat datang di **${member.guild.name}** ヾ(≧▽≦*)o`)
+        .setDescription(`Hei ${member.user.username}, Selamat datang di **${member.guild.name}** ヾ(≧▽≦*)o`)
 
         // Send Embed
-        channel.send({ embeds: [embed] })
+        channel.send({ content: `Hello <@${member.user.id}>!`, embeds: [embed] })
     } catch (error) {
         return console.log(error);
     }
