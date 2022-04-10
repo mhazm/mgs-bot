1e77bc502e2c6f2731006f100f9c6cef2e7b4d95
Muhammad Hazmi <mhazmiee@gmail.com>
2022-04-10 10:11:37 +0700

    Add Icon Thumbnail and Add Misc Timeout

--- commands/account/myrole.js
+++ commands/account/myrole.js
@@ -16,12 +16,15 @@ module.exports = {
     run: async(client, message, args) => {
         try {
             moment.locale('id');
+            const guildicon = message.guild.iconURL({ dynamic: true });
             let data = await CustomRole.findOne({
                 guildID: message.guild.id,
                 userID: message.author.id,
             });
     
-            if (!data) return message.reply(`Maaf, kamu ngga punya custom role. Jadi nggabisa pakai fitur ini`);
+            if (!data) return message.reply(`Maaf, kamu ngga punya custom role. Jadi nggabisa pakai fitur ini`).then(msg => {
+                setTimeout(() => msg.delete(), 5000)
+              });
             const myRole = message.guild.roles.cache.find(r => r.id === data.roleID);
             const createdDate = moment.tz(data.createdDate, 'Asia/Jakarta').format('LLLL');
 
@@ -32,10 +35,14 @@ module.exports = {
             if (!guild) return;
     
             if(!args[0]) {
+                let icon = myRole.iconURL()
+                if (!icon) {
+                    icon = guildicon;
+                }
                 let help = new MessageEmbed()
                 .setColor(myRole.color)
                 .setDescription(`
-                Kamu dapat menggunakan command ini dengan contoh seperti ini : ${guild.prefix}set <args> dan nanti tinggal jawab saja pertanyaan dari dapin.\n
+                Kamu dapat menggunakan command ini dengan contoh seperti ini : ${guild.prefix}mr <args> dan nanti tinggal jawab saja pertanyaan dari dapin.\n
                 Pilihan argumen yang tersedia adalah :
                 <a:kanan:819853363179945994> **List Setting**
                 • nama
@@ -43,13 +50,16 @@ module.exports = {
                 • icon
                 `)
                 .addField('Role Name', `<@&${myRole.id}>`)
-                .addField('Created Date', createdDate);
+                .addField('Created Date', createdDate)
+                .setThumbnail(icon);
                 return message.channel.send({ embeds: [help] })
             }
 
 
             if (args[0].toLowerCase() === 'nama') {
-                message.reply('Apa nama baru role kamu?');
+                message.reply('Apa nama baru role kamu?').then(msg => {
+                    setTimeout(() => msg.delete(), 30_000)
+                  });;
                 
                 const filter = m => m.content && m.author.id === message.author.id;
                 channel = message.channel;
@@ -59,7 +69,9 @@ module.exports = {
                 answer.on('collect', m => {
                     const finalanswer = m.content;
                     if (finalanswer.length > 50 ) {
-                        return message.reply('Nama Role kamu tidak boleh lebih dari 50 karakter!');
+                        return message.reply('Nama Role kamu tidak boleh lebih dari 50 karakter!').then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     }
     
                     myRole.edit({
@@ -69,18 +81,24 @@ module.exports = {
                             .setDescription(`<a:verified:962503696288215051> Berhasil mengubah nama role menjadi ${update.name}`)
                             .setTimestamp()
                             .setColor(client.config.berhasil)
-                        message.channel.send({ embeds: [embed] });
+                        message.channel.send({ embeds: [embed] }).then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     })
                     
                 });
                 
                 answer.on('end', m => {
                     if (m.size === 0) {
-                        message.reply('Kamu tidak memberikan jawaban.');
+                        message.reply('Kamu tidak memberikan jawaban.').then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     }               
                 })
             } else if (args[0].toLowerCase() === 'warna') {
-                message.reply('Apa warna baru role kamu?');
+                message.reply('Apa warna baru role kamu?').then(msg => {
+                    setTimeout(() => msg.delete(), 30_000)
+                  });
                 
                 const filter = m => m.content && m.author.id === message.author.id;
                 channel = message.channel;
@@ -90,7 +108,9 @@ module.exports = {
                 answer.on('collect', m => {
                     const finalanswer = m.content;
                     if (finalanswer.length > 50 ) {
-                        return message.reply('Nama Role kamu tidak boleh lebih dari 50 karakter!');
+                        return message.reply('Nama Role kamu tidak boleh lebih dari 50 karakter!').then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     }
     
                     myRole.edit({
@@ -100,18 +120,24 @@ module.exports = {
                             .setDescription(`<a:verified:962503696288215051> Berhasil mengubah warna role <@&${update.id}>`)
                             .setTimestamp()
                             .setColor(client.config.berhasil)
-                        message.channel.send({ embeds: [embed] });
+                        message.channel.send({ embeds: [embed] }).then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     })
                     
                 });
                 
                 answer.on('end', m => {
                     if (m.size === 0) {
-                        message.reply('Kamu tidak memberikan jawaban.');
+                        message.reply('Kamu tidak memberikan jawaban.').then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     }               
                 })
             } else if (args[0].toLowerCase() === 'icon') {
-                message.reply('Apa icon baru role kamu?');
+                message.reply('Apa icon baru role kamu?').then(msg => {
+                    setTimeout(() => msg.delete(), 30_000)
+                  });
                 
                 const filter = m => m.content && m.author.id === message.author.id;
                 channel = message.channel;
@@ -121,7 +147,9 @@ module.exports = {
                 answer.on('collect', m => {
                     const finalanswer = m.content;
                     if (finalanswer.length > 100 ) {
-                        return message.reply('Nama Role kamu tidak boleh lebih dari 50 karakter!');
+                        return message.reply('Nama Role kamu tidak boleh lebih dari 50 karakter!').then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     }
     
                     myRole.edit({
@@ -137,7 +165,9 @@ module.exports = {
                 
                 answer.on('end', m => {
                     if (m.size === 0) {
-                        message.reply('Kamu tidak memberikan jawaban.');
+                        message.reply('Kamu tidak memberikan jawaban.').then(msg => {
+                            setTimeout(() => msg.delete(), 5000)
+                          });
                     }               
                 })
             }
