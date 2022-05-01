4fc3dfb05c97627c1307bf9d1979ef2e0b9dc0d8
Muhammad Hazmi <mhazmiee@gmail.com>
2022-05-01 21:23:47 +0700

    Event Budget

--- commands/settings/setbudget.js
+++ commands/settings/setbudget.js
@@ -1,4 +1,4 @@
-const { Client, Message, MessageEmbed, Permissions } = require('discord.js');
+const { Client, Message, MessageEmbed } = require('discord.js');
 const Guild = require('../../models/Guild');
 
 module.exports = {
@@ -10,10 +10,10 @@ module.exports = {
      */
     run: async(client, message, args) => {
         try {
-            if (!message.member.permissions.has([Permissions.FLAGS.ADMINISTRATOR]))
-                return message.reply(`Siapa lu woy!!`).then(msg => {
-                    setTimeout(() => msg.delete(), 3000)
-                });
+            if (message.author.id !== process.env.OWNERID)
+            return message.reply(`Siapa lu woy!!`).then(msg => {
+                setTimeout(() => msg.delete(), 3000)
+            });
 
             let guild = await Guild.findOne({
                 guildID: message.guild.id
@@ -72,6 +72,36 @@ module.exports = {
                 guild.budget = 0;
                 guild.save();
                 message.channel.send(`Budget telah direset menjadi 0.`).then(msg => {
+                    setTimeout(() => msg.delete(), 3000)
+                });
+            } else if (args[0].toLowerCase() === 'event-add') {
+                let notif = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setDescription(`Budget server telah ditambahkan Rp.${budget}\nSaat ini budget event sebesar Rp.${guild.eventbudget}`)
+                    .setTimestamp()
+                    .setFooter({text: `Request by ${message.author.username}`, iconURL: message.author.displayAvatarURL({ dynamic: true })})
+                modlog.send({ embeds: [notif] });
+
+                guild.eventbudget += budget;
+                guild.save();
+                message.channel.send(`Budget telah berhasil ditambahkan senilai Rp.${budget}`).then(msg => {
+                    setTimeout(() => msg.delete(), 3000)
+                });
+            } else if (args[0].toLowerCase() === 'event-remove') {
+                if (guild.eventbudget < budget) {
+                    return message.reply('Budget servernya udah ga ada, masih aja mau dikurangin..')
+                }
+
+                let notif = new MessageEmbed()
+                    .setColor("GREEN")
+                    .setDescription(`Budget server telah dikurangkan Rp.${budget}\nSaat ini budget payout sebesar Rp.${guild.budget}`)
+                    .setTimestamp()
+                    .setFooter({text: `Request by ${message.author.username}`, iconURL: message.author.displayAvatarURL({ dynamic: true })})
+                modlog.send({ embeds: [notif] });
+
+                guild.eventbudget -= budget;
+                guild.save();
+                message.channel.send(`Budget telah berhasil dikurangkan senilai Rp.${budget}`).then(msg => {
                     setTimeout(() => msg.delete(), 3000)
                 });
             }
